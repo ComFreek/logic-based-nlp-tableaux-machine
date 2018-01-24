8b71d9be62adaeadc27683ff5d983e153818216a
ze26zefo <navid.roux@fau.de>
2018-01-24 09:48:46 +0100

    Add automated LaTeX generation for all tests

--- README.md
+++ README.md
@@ -1,20 +1,43 @@
 # TableauxMachine
 
-Implemented as an exercise for the course ["Logik-Basierte Sprachverarbeitung" (LBS)](https://kwarc.info/courses/lbs/).
+Theorem prover and model generator for [first-order logic](https://en.wikipedia.org/wiki/First-order_logic).
 
-## Usage
+Implemented as an exercise for the course ["Logik-Basierte Sprachverarbeitung" (LBS)](https://kwarc.info/courses/lbs/)
+at the [Friedrich-Alexander University Erlangen-Nuremberg](https://www.fau.de/) in the winter
+term 2017/2018.
+
+Supported rules:
+
+- `(a ∨ b)^T`
+- `(a ∨ b)^F`
+- `(a = b)^T` in conjunction with `ϕ = ψ`, of them containing `a` or `b`.
+- (RM: ∀): `(∀x.ϕ)^T`
+- (RM: ∃): `(∀x. ϕ)^F`
+
+## Installation
 
 1. Clone
 2. If you are not using Windows: add the compiled GF `*.so` libraries in `lib/`.
 3. Install [SBT](https://www.scala-sbt.org/) if you do not have it already installed. (Check `sbt` on the command line.)
-4. Run `sbt` in the root directory and then enter
+
+## Usage
+
+1. Run `sbt` in the root directory and then enter
    - `test` for all tests (might run several minutes)
    - `testOnly -- -l org.scalatest.tags.Slow` to exclude slow tests.
 
+2. For every run test corresponding LaTeX files with the tableaux graph will be
+created in the directory `test-models-output/latex`.<br><br>
+   Note that due to the use of the specific LaTeX package for the visualizations,
+   nodes with one child do not display their branch visually, they are simply
+   concatenated. This might happen in conjunction with (RM: ∃).
 
-## Developing
+
+## Developing tips
 
 - Inside SBT, you can use `testOnly -- -z "xyz"` to only run the tests whose names contain "xyz".
+- Looking at the generated graphs in `test-models-output/latex` is recommended to catch
+  bugs when implementing new functionality.
 - If tests are failing and manual comparisons between models (i.e. between Maps) is cumbersome, you can temporarily use the following code:
 
       println(modelToString(machine.nextModel().get))
--- src/test/scala/de/fau/cs/gitlab/ze26zefo/TableauxMachine/ModelGeneratorSpec.scala
+++ src/test/scala/de/fau/cs/gitlab/ze26zefo/TableauxMachine/ModelGeneratorSpec.scala
@@ -1,16 +1,84 @@
 package de.fau.cs.gitlab.ze26zefo.TableauxMachine
 
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+
 import info.kwarc.mmt.api._
 import info.kwarc.mmt.api.objects._
 import info.kwarc.mmt.api.utils.URI
 import org.scalatest._
 import info.kwarc.gf.Convenience.{Pred1, Pred2, forall => termForall, not => termNot, or => termOr}
 
+/**
+  * Common Spec functions and traits for suites testing the GraphTableauxMachine or
+  * - more generally - a ModelGenerator.
+  *
+  * Especially, you can use the Machine trait specified herein to use a freshly
+  * created ModelGenerator instance:
+  *
+  * <code>
+  *   it should "your test name" in new Machine {
+  *     machine.feed(/* ... */);
+  *   }
+  * </code>
+  *
+  * If TRACK_TEST_MODELS_LATEX is set, in each test upon a call of nextModel(), the
+  * model is generated, intercepted and exported as a LaTeX file in the paths
+  * given by TEST_MODELS_DIR and TEST_MODELS_LATEX_DIR. The LaTeX file name depends
+  * on the test name.
+  */
 abstract class ModelGeneratorSpec extends FlatSpec with Matchers {
 
+  private val TRACK_TEST_MODELS_LATEX = true
+  private val TEST_MODELS_DIR = Paths.get("test-models-output")
+  private val TEST_MODELS_LATEX_DIR = TEST_MODELS_DIR.resolve("latex")
+
   trait Machine {
-    val machine: ModelGenerator = new GraphTableauxMachine()
+    private val actualMachine: GraphTableauxMachine = new GraphTableauxMachine()
+    private var nextModelCounter = 1
+
+    val machine: ModelGenerator = if (TRACK_TEST_MODELS_LATEX) {
+      new ModelGenerator {
+        override def feed(term: Term): Unit = {
+          actualMachine.feed(term)
+        }
+
+        override def nextModel(): Option[Model] = {
+          val model = actualMachine.nextModel()
+
+          // Escape for filename use
+          val escapedTestName = _currentTestName.get().replaceAll("[^\\w ]+", "").replace(' ', '-')
+          val latexFile = TEST_MODELS_LATEX_DIR.resolve(escapedTestName + "-" + nextModelCounter + ".tex")
+
+          Files.write(
+            latexFile,
+            actualMachine.generateLatexDocument().getBytes(StandardCharsets.UTF_8)
+          )
+
+          nextModelCounter += 1
+          model
+        }
+      }
+    }
+    else {
+      actualMachine
+    }
   }
+
+  // Save the current test name to use in LaTeX export upon nextModel()
+  //
+  // Copied from https://stackoverflow.com/a/36766748
+  // Author: Alexey Romanov <https://stackoverflow.com/users/9204/alexey-romanov>
+  // License: CC BY-SA 3.0 with attribution required
+  private val _currentTestName = new ThreadLocal[String]
+
+  override def withFixture(test: NoArgTest): Outcome = {
+    _currentTestName.set(test.name)
+    val outcome = super.withFixture(test)
+    _currentTestName.set(null)
+    outcome
+  }
+  // End code for saving the current test name.
 
   // For debugging purposes
   protected def modelToString(model: Model): String = {
